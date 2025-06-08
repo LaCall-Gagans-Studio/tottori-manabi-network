65b75f26281972e65682108cd214afdf49f8aca4
tohma.A <tohma.a.hello.jp@gmail.com>
2025-06-08 22:45:35 +0900

    a

--- tottori-manabi-network-app/next.config.mjs
+++ tottori-manabi-network-app/next.config.mjs
@@ -3,6 +3,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  experimental: {
+    outputFileTracingIgnores: ['**/.next/cache/**'],
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
