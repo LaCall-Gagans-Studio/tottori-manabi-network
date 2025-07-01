668e36705c17d920b49931a49bba5464b032c642
GAGANS STUDIO <studio@lacall-gagans.com>
2025-07-01 16:35:55 +0900

    word block fixed

--- tottori-manabi-network-app/src/app/components/section.dict/wordCloudBlock.tsx
+++ tottori-manabi-network-app/src/app/components/section.dict/wordCloudBlock.tsx
@@ -3,10 +3,16 @@
 import React, { useEffect, useRef } from 'react'
 import cloud, { Word } from 'd3-cloud'
 import * as d3 from 'd3'
+import { schemeCategory10 } from 'd3-scale-chromatic'
 
 interface KeywordItem {
   id: string
   keyword: string
+}
+
+// Word型を拡張してvalueを追加
+interface MyWord extends Word {
+  value: number
 }
 
 interface WordCloudCanvasProps {
@@ -19,7 +25,7 @@ const WordCloudCanvas: React.FC<WordCloudCanvasProps> = ({ keywords }) => {
   useEffect(() => {
     if (!svgRef.current) return
 
-    const words: Word[] = keywords.map((item, index, array) => {
+    const words: MyWord[] = keywords.map((item, index, array) => {
       const text = typeof item === 'string' ? item : item.keyword
       return {
         text,
@@ -30,20 +36,20 @@ const WordCloudCanvas: React.FC<WordCloudCanvasProps> = ({ keywords }) => {
     const width = 300
     const height = 200
 
-    const layout = cloud<Word>()
+    const layout = cloud<MyWord>()
       .size([width, height])
       .words(words)
       .padding(5)
       .rotate(() => 0)
       .font('Zen Kaku Gothic New')
-      .fontSize((d) => d.value as number)
+      .fontSize((d) => d.value)
       .on('end', draw)
 
     layout.start()
 
-    function draw(words: Word[]) {
+    function draw(words: MyWord[]) {
       const svg = d3
-        .select(svgRef.current as EventTarget)
+        .select(svgRef.current as EventTarget as SVGSVGElement)
         .attr('width', width)
         .attr('height', height)
 
@@ -57,10 +63,10 @@ const WordCloudCanvas: React.FC<WordCloudCanvasProps> = ({ keywords }) => {
         .enter()
         .append('text')
         .style('font-size', (d) => `${d.size}px`)
-        .style('fill', () => d3.schemeCategory10[Math.floor(Math.random() * 10)])
+        .style('fill', () => schemeCategory10[Math.floor(Math.random() * 10)] ?? '#000')
         .attr('text-anchor', 'middle')
         .attr('transform', (d) => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
-        .text((d) => d.text)
+        .text((d) => d.text ?? '')
     }
   }, [keywords])
 
